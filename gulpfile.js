d637b2c9d4dd3d46206b1371abf5cb580a646240
Atishay <atishaybaid@gmail.com>
2016-04-02 20:51:23 +0530

    (gulp)less,watch task added

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,22 @@
 var gulp = require('gulp');
+var less = require('gulp-less');
+var path = require('path');
+var watch = require('gulp-watch');
 
 
-gulp.task('default',function(){
-    console.log("gulp configured");
+gulp.task('less',function(){
+    return gulp.src('./assets/less/*.less')
+    .pipe(less({
+      paths: [ path.join(__dirname, 'less', 'includes') ]
+    }))
+    .pipe(gulp.dest('./public/css'));
+
+});
+
+
+gulp.task('watch',function(){
+    gulp.watch('./assets/less/*.less',['less']);
 })
+
+
+gulp.task('default',['less']);
